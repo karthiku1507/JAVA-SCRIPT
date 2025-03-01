6e01e39d1db42f6c88c5ee4794457f9231b378e1
karthiku1507 <karthiku1507@gmail.com>
2025-03-01 18:29:20 +0530

    dom

---
+++ DOM/changepage/DOM.js
@@ -0,0 +1,10 @@
+function changeContent() {
+    var pDomelement = document.querySelector("#pera");
+    var h2Domelement = document.querySelector(".title");
+    pDomelement.innerText = "adavance javascript is bakend work ";
+    h2Domelement.innerText = "javascript is backend";
+    h2Domelement.style.color = "red";
+    h2Domelement.style.backgroundColor= "pink"
+    pDomelement.style.color = "blue"
+    
+}
