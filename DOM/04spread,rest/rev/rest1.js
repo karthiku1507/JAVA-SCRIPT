eeb9bddbec43d10d2a7ba05c87215a201ab28138
karthiku1507 <karthiku1507@gmail.com>
2025-08-11 18:40:46 +0530

    add

---
+++ DOM/04spread,rest/rev/rest1.js
@@ -0,0 +1,7 @@
+function myFun(p1,...args){
+    console.log(p1);
+    console.log(args)
+
+}
+
+myFun(100,200,300,400,500)
---
+++ DOM/04spread,rest/rev/spreadarr.js
@@ -0,0 +1,26 @@
+var obj1 = {
+    name : "karthik",
+    city : "ongole",
+    Number: 7095350125,
+};
+
+var obj2 = {
+
+};
+
+
+console.log(obj1);
+console.log("=======================");
+console.log(obj2)
+
+
+
+
+var arr1 = [10,20,30,40,50]
+
+var arr2 = ["karthik","ukvk",...arr1,true,false]
+
+
+console.log(arr1);
+console.log("=========================")
+console.log(arr2)
